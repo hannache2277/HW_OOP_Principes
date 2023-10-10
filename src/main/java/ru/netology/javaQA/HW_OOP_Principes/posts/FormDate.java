2795119fd004d073db75413aa61c1e73ed7a0d48
hannache2277 <hannache2277@gmail.com>
2023-10-10 07:37:36 +0300

    add Java class Post and FormDate

---
+++ src/main/java/ru/netology/javaQA/HW_OOP_Principes/posts/FormDate.java
@@ -0,0 +1,8 @@
+package ru.netology.javaQA.HW_OOP_Principes.posts;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+}
--- src/main/java/ru/netology/javaQA/HW_OOP_Principes/posts/Post.java
+++ src/main/java/ru/netology/javaQA/HW_OOP_Principes/posts/Post.java
@@ -1,4 +1,12 @@
 package ru.netology.javaQA.HW_OOP_Principes.posts;
 
 public class Post {
+    public String name;
+    public String surname;
+    public FormDate birthday;
+    public String pasport;
+    public String patronymic;
+    public String phone;
+    public boolean subscription;
+
 }
